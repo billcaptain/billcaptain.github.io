692a81837a991ad7cee6e94d0c72c9951714eee2
billcaptain <122194414@qq.com>
2016-10-17 15:49:18 +0800

    20161017

--- js/ex_js001.js
+++ js/ex_js001.js
@@ -1,3 +1,11 @@
 function myFunction(){
-    console.log('Hello');
+    //console.log('Hello');
+   // $("div#output").text("Hello");
+    var str = $("#input").val();
+    $("#output").text(str);    
+}
+$("#test").bind('click',myFunction);
+function myFunction1(){
+    console.log('ByeBye');
+    $("div#output1").text("ByeBye");
 }
